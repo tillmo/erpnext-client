96160c4898505595d42015af7c6e77ab4a431bbe
Till Mossakowski <till.mossakowski@ovgu.de>
2021-06-02 19:39:40 +0200

    absolut path

--- erpnext39.bat
+++ erpnext39.bat
@@ -1,1 +1,1 @@
-\Users\ASUS\AppData\Local\Programs\Python\Python39\python.exe Desktop\erpnext-client\erpnext.py
+\Users\ASUS\AppData\Local\Programs\Python\Python39\python.exe \Users\ASUS\Desktop\erpnext-client\erpnext.py
