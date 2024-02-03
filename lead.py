c5bb0a4a6b29c030310508e3019da8dd41b9c550
Till Mossakowski <till@communtu.de>
2024-02-03 20:21:16 +0100

    mark known leads as "do not contact"

--- lead.py
+++ lead.py
@@ -2,6 +2,14 @@ from api import Api, LIMIT
 from settings import LEAD_OWNERS
 import utils
 import easygui
+import json
+
+def is_change_into_not_contact(v):
+    if 'data' in v:
+        j = json.loads(v['data'])
+        if j.get('changed') == [['status', 'Open', 'Do Not Contact']]:
+            return True
+    return False    
 
 def process_open_leads():
     cleanup_leads()
@@ -20,14 +28,22 @@ def process_open_leads():
     for lead1 in leads:
         #print(lead1['lead_owner'])
         res = Api.api.load_doc("Lead",lead1['name'])
-        doc = res['docs'][0]
-        comms = res['docinfo']['communications']
-        title = "Bitte Lead Owner wählen"
-        texts = [utils.html_to_text(comm['content']) for comm in comms]
-        text = "\n--------------------\n".join(texts)
-        text = "\n".join(text.split("\n")[:35])[:1000]
-        msg = f"{doc['name']}   {doc['lead_name']}\n\n{text}"
-        choice = easygui.choicebox(msg, title, choices)
+        versions = res['docinfo']['versions']
+        choice = None
+        for v in versions:
+            if is_change_into_not_contact(v):
+                choice = 'kein Lead'
+                print(f'Markiere Lead {lead1["name"]} wieder als "nicht kontaktieren"')
+                break
+        if not choice:
+            doc = res['docs'][0]
+            comms = res['docinfo']['communications']
+            title = "Bitte Lead Owner wählen"
+            texts = [utils.html_to_text(comm['content']) for comm in comms]
+            text = "\n--------------------\n".join(texts)
+            text = "\n".join(text.split("\n")[:35])[:1000]
+            msg = f"{doc['name']}   {doc['lead_name']}\n\n{text}"
+            choice = easygui.choicebox(msg, title, choices)
         if choice is None:
             print("Lead-Bearbeitung abgebrochen")        
             return
