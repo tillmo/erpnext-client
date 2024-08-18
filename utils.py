fd8be5c3af561f224289a64db695067b3df65721
Till Mossakowski <till@communtu.de>
2024-08-18 22:14:03 +0200

    evince in background also for Windows

--- utils.py
+++ utils.py
@@ -236,32 +236,8 @@ def extract_prnr(text):
         return prnr.group(1)
     return None
 
-def find_evince_in_registry():
-    import winreg
-    try:
-        with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall") as key:
-            for i in range(0, winreg.QueryInfoKey(key)[0]):
-                sub_key_name = winreg.EnumKey(key, i)
-                with winreg.OpenKey(key, sub_key_name) as sub_key:
-                    try:
-                        display_name = winreg.QueryValueEx(sub_key, "DisplayName")[0]
-                        if "Evince" in display_name:
-                            install_location = winreg.QueryValueEx(sub_key, "InstallLocation")[0]
-                            return os.path.join(install_location, "bin", "evince.exe")
-                    except FileNotFoundError:
-                        continue
-    except FileNotFoundError:
-        pass
-    return None
-
 def evince(filename):
-    if running_linux():
-        os.system(f'evince {filename} &')
-    else:
-        evince_path = find_evince_in_registry()
-        if evince_path:
-            subprocess.run([evince_path, filename])
-        else:
-            print("Evince not found in registry.")
-            print("Please install Evince or set the path to the Evince executable in the registry.")
-
+    subprocess.Popen(["evince", filename],
+                     stdout=subprocess.PIPE,
+                     stderr=subprocess.PIPE,
+                     stdin=subprocess.PIPE)
