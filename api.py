3a9bbf89e1191efb0db4de93818537d7362ebfad
Till Mossakowski <till@communtu.de>
2026-04-18 19:24:21 +0200

    read in items faster

--- api.py
+++ api.py
@@ -37,26 +37,57 @@ class Api(object):
             Api.items_by_code = {}
             Api.item_code_translation = defaultdict(lambda: {})
             company_name = sg.UserSettings()['-company-']
-            items = Api.api.get_list('Item',limit_page_length=LIMIT,
-                                     filters={'disabled':0},
-                                     fields=['item_code','item_name'])
-            print("Lese alle {} ERPNext-Artikel ein".format(len(items)),end="")
+            items = Api.api.get_list('Item', limit_page_length=LIMIT,
+                                     filters={'disabled': 0},
+                                     fields=['name', 'item_code', 'item_name',
+                                             'item_group', 'description'])
+            print("Lese alle {} ERPNext-Artikel ein".format(len(items)))
             for item in items:
-                print(".",end="")
-                item_code = item["item_code"]
-                doc = Api.api.get_doc('Item', item_code)
-                Api.items_by_code[item_code] = doc
-                if not doc['item_defaults']:
-                    doc['item_defaults'] = [{'company': company_name,
-                                             'default_warehouse': WAREHOUSE}]
-                    gui_api_wrapper(Api.api.update,doc)
-                for supplier_items in doc['supplier_items']:
-                    if 'supplier_part_no' in supplier_items:
-                        supplier = supplier_items['supplier']
-                        supplier_part_no = supplier_items['supplier_part_no']
-                        Api.item_code_translation[supplier][supplier_part_no] =\
-                                                  item_code
-            print()
+                item['supplier_items'] = []
+                Api.items_by_code[item['item_code']] = item
+            # Join child table Item Supplier via parent doctype to fetch all
+            # supplier mappings in a single request (child doctypes cannot be
+            # queried directly via the REST API).
+            supplier_rows = Api.api.get_list(
+                'Item', limit_page_length=LIMIT,
+                filters={'disabled': 0},
+                fields=['item_code',
+                        '`tabItem Supplier`.supplier as si_supplier',
+                        '`tabItem Supplier`.supplier_part_no as si_supplier_part_no'])
+            for row in supplier_rows:
+                if not row.get('si_supplier_part_no'):
+                    continue
+                item = Api.items_by_code.get(row['item_code'])
+                if not item:
+                    continue
+                supplier = row['si_supplier']
+                supplier_part_no = row['si_supplier_part_no']
+                item['supplier_items'].append(
+                    {'supplier': supplier,
+                     'supplier_part_no': supplier_part_no})
+                Api.item_code_translation[supplier][supplier_part_no] = item['item_code']
+            # Same trick for Item Default - one row per (item, default) entry;
+            # we only need to know which items have a default for the current
+            # company so we can seed the rest below.
+            defaults_rows = Api.api.get_list(
+                'Item', limit_page_length=LIMIT,
+                filters={'disabled': 0},
+                fields=['name', '`tabItem Default`.company as default_company'])
+            items_with_defaults = {row['name'] for row in defaults_rows
+                                   if row.get('default_company') == company_name}
+            missing_defaults = [item for item in items
+                                if item['name'] not in items_with_defaults]
+            if missing_defaults:
+                print("Ergänze item_defaults für {} Artikel".format(len(missing_defaults)),
+                      end="")
+                for item in missing_defaults:
+                    print(".", end="")
+                    doc = Api.api.get_doc('Item', item['name'])
+                    if not doc['item_defaults']:
+                        doc['item_defaults'] = [{'company': company_name,
+                                                 'default_warehouse': WAREHOUSE}]
+                        gui_api_wrapper(Api.api.update, doc)
+                print()
             
     @classmethod
     def load_account_data(cls):
